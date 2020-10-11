befaf73d6cb9826486d3a2acc00e381f25cb2adf
Akash Dakoor <akashdakoor@gmail.com>
2020-10-11 17:16:20 +0530

    Add skeleton code for main and header file

---
+++ veclib.h
@@ -0,0 +1,31 @@
+#ifndef VECLIB_H
+#define VECLIB_H
+
+#define DIM 4
+// vector type
+typedef float Vector[DIM]	;
+
+// functions
+// print vector
+void print(Vector);
+// vector addition
+void add(Vector, Vector, Vector);
+// elementwise product
+void ele_Prod(Vector, Vector, Vector);
+// difference of two vectors
+void diff(Vector, Vector, Vector);
+// dot product
+float dot_Prod(Vector, Vector);
+// angle
+float angle(Vector, Vector);
+
+// defining print function
+void print(Vector vec_a) 
+{
+	printf("[ ");
+	for (int i=0; i<DIM; i++)
+		printf("%.2f ", vec_a[i]);
+	printf("]\n");
+};
+
+#endif
---
+++ vecmain.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "veclib.h"
+
+int main(void)
+{
+    // declare and initialise the two vectors from stdin
+    Vector v1, v2, vRes;
+    printf("Enter the first vector\n");
+    for(int i=0; i<4; ++i)
+    {
+        scanf("%f", &v1[i]);
+    }
+    printf("Enter the second vector\n");
+    for(int i=0; i<4; ++i)
+    {
+        scanf("%f", &v2[i]);
+    }
+
+    // printing sum
+    // add(v1, v2, vRes);
+    // printf("The sum is:\n");
+    // print(vRes);
+    // printing elementwise product
+    // elem_Prod(v1, v2, vRes);
+    // printf("The elementwise product is:\n");
+    // print(vRes);
+    // printing difference
+    // diff(v1, v2, vRes);
+    // printf("The difference is:\n");
+    // print(vRes);
+    // printing dot product
+    // printf("The dot product is %.2f\n", dot_Prod(v1, v2));
+    // printing angle between the two vectors
+    // printf("The angle between the two vectors is %.2f\n", angle(v1, v2));
+    return 0;
+}
